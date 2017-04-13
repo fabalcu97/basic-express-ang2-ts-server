93b91201624c0eb7a41545f58a7f14b06e5ee874
fabalcu97 <fballoncuadros@gmail.com>
2017-04-13 10:24:58 -0500

    Updated gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,6 @@
 	var path = require('path');
 	var open = require('open');
 	var runSequence = require('run-sequence');
-	var ts = require('gulp-typescript');
-	var package = require('./package.json');
 	var tsconfig = require('./tsconfig.json');
 	var fs = require('fs-extra');
 	var replace = require("replace");
@@ -13,31 +11,6 @@
 /**
  * Items
  */
-
-	// Dependencies
-		gulp.task('typings:install', function(module, global) {
-			if (!module) {
-				shell.exec('node_modules/.bin/typings install');
-			} else {
-				shell.exec(
-					'node_modules/.bin/typings install '+module+' --save'+
-					(global ? ' --global' : '')
-				);
-			}
-		});
-
-		gulp.task('typings:search', function(module) {
-			if (!module) return;
-			shell.exec('node_modules/.bin/typings search '+module);
-		});
-
-		gulp.task('typings:uninstall', function(module) {
-			if (!module) return;
-			shell.exec(
-				'node_modules/.bin/typings uninstall '+module+' --save'+
-				(global ? ' --global' : '')
-			);
-		});
 
 	// Servers
 		gulp.task('server:start', function () {
@@ -54,27 +27,23 @@
 	// Build
 		gulp.task('build', function () {
 			runSequence([
-				'server:build',
+				'server:build'
 			]);
 		});
-	
+
 	// Serve
 		gulp.task('serve', function (build, buildAll) {
 			var sequence = [];
 			if (build || buildAll) {
 				sequence.push('server:build');
 			}
-			if (buildAll) {
-				//sequence.push('webapps:build');
-			}
 			sequence.push('server:start');
 			runSequence(sequence);
 		});
-	
+
 	// Init
 		gulp.task('init', function () {
 			runSequence([
-				'typings:install',
 				'build'
 			]);
-		})
+		});
